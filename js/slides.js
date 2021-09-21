4ef531bb3ff139ffceaa9a78186720dfbedaea14
llDanielll5 <danixmotaa7x@gmail.com>
2021-09-21 15:07:57 -0300

    landingPage04

---
+++ js/slides.js
@@ -0,0 +1,71 @@
+$(function(){
+    //APLICANDO SLIDE COM PLUGIN SLICK SLIDE
+    $('.mosaico .container .mosaico-wraper').slick({
+        certerMode:false,
+        slidesToShow: 6,
+        arrow: false,
+        responsive:[
+            {
+                breakpoint: 768,
+                settings:{
+                    arrows:false,
+                    centerMode: true,
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 580,
+                settings:{
+                    slidesToShow:2,
+                    arrows: false,
+                    centerMode: true
+                }
+            },
+            {
+                breakpoint: 380,
+                settings:{
+                    arrows: false,
+                    centerMode: true,
+                    slidesToShow:1
+                }
+            }
+        ]
+    });
+
+    $('.tratamentos .container').slick({
+        centerMode: false,
+        slidesToShow: 3,
+        arrows: false,
+        infinite: false,
+        responsive:[
+            {
+                breakpoint: 768,
+                settings:{
+                    arrows: false,
+                    dots: true,
+                    infinite: false,
+                    centerMode: false,
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint:480,
+                settings:{
+                    arrows: false,
+                    dots:true,
+                    infinite: false,
+                    centerMode: false,
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+    $('.depoimentos .container').slick({
+        centerMode: false,
+        slidesToShow: 1,
+        arrows: false,
+        dots: true,
+        infinite: false
+    });
+})
